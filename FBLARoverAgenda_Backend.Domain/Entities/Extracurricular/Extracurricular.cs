39067f6be7cd40cc46a871a56c9a42e0ed15ff24
Vincent Lauro <vinniehat@gmail.com>
2022-04-01 23:46:14 -0400

    Added Extracurriculars

---
+++ FBLARoverAgenda_Backend.Domain/Entities/Extracurricular/Extracurricular.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBLARoverAgenda_Backend.Domain.Entities.Extracurricular
+{
+    public class Extracurricular
+    {
+        [Key] public string Id { get; set; }
+        
+        [Required] public string Name { get; set; }
+        [Required] public string Description { get; set; }
+        [Required] public List<DateTime> MeetingDates { get; set; }
+
+        [Required] public Teacher.Teacher Teacher { get; set; }
+        [Required] public string TeacherId { get; set; }
+
+
+    }
+}
---
+++ FBLARoverAgenda_Backend.Infrastructure/Persistence/Migrations/20220402030708_AddedExtracurriculars.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FBLARoverAgenda_Backend.Infrastructure.Persistence.Migrations
+{
+    public partial class AddedExtracurriculars : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
