d1564d16aa561c7d4983c2fb6d3171fcc68ed736
Vincent Lauro <vinniehat@gmail.com>
2022-06-12 17:44:57 -0400

    Added LunchMenuItem Model

---
+++ FBLARoverAgenda_Backend.Domain/Entities/LunchMenuItem/LunchMenuItem.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+
+namespace FBLARoverAgenda_Backend.Domain.Entities;
+
+public class LunchMenuItem
+{
+    [Required]
+    public string Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public DateTime StartTime { get; set; }
+    [Required]
+    public DateTime EndTime { get; set; }
+}
