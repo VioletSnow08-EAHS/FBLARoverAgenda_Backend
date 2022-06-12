3b10590f5a39c174c51aa0aea288bb3692a65952
Vincent Lauro <vinniehat@gmail.com>
2022-06-12 17:44:40 -0400

    Added ClassesController

---
+++ FBLARoverAgenda_Backend.Web/Areas/Api/Controllers/ClassesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FBLARoverAgenda_Backend.Domain.Entities.FAQ;
+using FBLARoverAgenda_Backend.Infrastructure.Persistence.DbContexts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FBLARoverAgenda_Backend.Web.Areas.Api.Controllers;
+
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Produces("application/json")]
+[Area("Api")]
+public class ClassesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ClassesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    // GET
+    public List<FAQ> Index()
+    {
+        return _context.FAQs.ToList();
+    }
+}
