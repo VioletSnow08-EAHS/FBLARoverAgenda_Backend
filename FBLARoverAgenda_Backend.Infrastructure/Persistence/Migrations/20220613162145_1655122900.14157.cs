4ba2394daf31be149a9ab4dc9df2af6e4645bcfa
Vincent Lauro <vinniehat@gmail.com>
2022-06-13 12:22:27 -0400

    Added Class Model Migration

---
+++ FBLARoverAgenda_Backend.Infrastructure/Persistence/Migrations/20220613162145_1655122900.14157.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FBLARoverAgenda_Backend.Infrastructure.Persistence.Migrations
+{
+    public partial class _165512290014157 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Suffix",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Suffix",
+                table: "Teachers");
+        }
+    }
+}
