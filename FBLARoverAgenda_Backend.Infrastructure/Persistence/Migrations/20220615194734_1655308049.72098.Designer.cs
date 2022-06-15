49cfeadfbfdfb3cbe4ee4fb56ea333dd3f628227
Vincent Lauro <vinniehat@gmail.com>
2022-06-15 15:48:22 -0400

    Added LunchMenuItem and SchoolEvent MigrationB

--- FBLARoverAgenda_Backend.Infrastructure/Persistence/Migrations/20220615194734_1655308049.72098.Designer.cs
+++ FBLARoverAgenda_Backend.Infrastructure/Persistence/Migrations/20220615194734_1655308049.72098.Designer.cs
@@ -4,6 +4,7 @@ using FBLARoverAgenda_Backend.Infrastructure.Persistence.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FBLARoverAgenda_Backend.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220615194734_1655308049.72098")]
+    partial class _165530804972098
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
