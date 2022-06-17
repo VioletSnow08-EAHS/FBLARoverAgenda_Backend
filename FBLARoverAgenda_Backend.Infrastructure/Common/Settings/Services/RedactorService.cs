2dbb99cfb2c9ce63668b4b2f15e10dda22f2e0c7
Vincent Lauro <vinniehat@gmail.com>
2022-06-17 18:52:37 -0400

    Created the RedactorService

---
+++ FBLARoverAgenda_Backend.Infrastructure/Common/Settings/Services/RedactorService.cs
@@ -0,0 +1,18 @@
+﻿using FBLARoverAgenda_Backend.Domain.Entities.Identity;
+
+namespace FBLARoverAgenda_Backend.Infrastructure.Common.Settings.Services;
+
+public class RedactorService
+{
+    public static ApplicationUser REDACT(ApplicationUser user)
+    {
+        user.PasswordHash = "[REDACTED]";
+        user.ConcurrencyStamp = "[REDACTED]";
+        user.LockoutEnabled = false;
+        user.LockoutEnd = null;
+        user.AccessFailedCount = 0;
+        user.ConcurrencyStamp = "[REDACTED]";
+        user.Claims = null;
+        return user;
+    }
+}
